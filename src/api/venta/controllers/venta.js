0910144b1f6fea06a21c623514ed100541eeb977
Tino Reyna <tinoreyna1984@gmail.com>
2022-11-01 15:45:01 -0500

    first commit

---
+++ src/api/venta/controllers/venta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * venta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::venta.venta');
---
+++ src/api/venta/routes/venta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * venta router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::venta.venta');
