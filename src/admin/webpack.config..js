eb059af4d2200076c69fd0b9076a60e5ef0fe60a
Tino Reyna <tinoreyna1984@gmail.com>
2022-11-02 14:30:37 -0500

    Se agrega funcionalidad de migracion de data

---
+++ src/admin/webpack.config..js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* eslint-disable no-unused-vars */
+
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  config.plugins.push(new MonacoWebpackPlugin());
+  return config;
+};
